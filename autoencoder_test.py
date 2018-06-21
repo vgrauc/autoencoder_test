bbbe574b8ba458c7d47b60dd9bff1edbf513dda6
vgrauc <vgrauc@gmail.com>
2018-06-21 16:05:35 +0100

    Update autoencoder_test.py

--- autoencoder_test.py
+++ autoencoder_test.py
@@ -1,1 +1,31 @@
+import matplotlib
+import matplotlib.pyplot as plt
 
+from keras.layers import Input, Dense
+from keras.models import Model
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats (28x28)
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input=input_img, output=decoded)
+print("autoencoder model created")
+
+# this model maps an input to its encoded representation
+encoder = Model(input=input_img, output=encoded)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
