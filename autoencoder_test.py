826a2a3fda62396d56585871779f26adc205d481
vgrauc <vgrauc@gmail.com>
2018-06-21 16:06:08 +0100

    Update autoencoder_test.py

--- autoencoder_test.py
+++ autoencoder_test.py
@@ -29,3 +29,25 @@ decoder_layer = autoencoder.layers[-1]
 decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
 
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+
+from keras.datasets import mnist
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# plt.imshow(x_train[2001], cmap='gray')
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+autoencoder.fit(x_train, x_train,
+                nb_epoch=5,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
